77d74c3a487319437161904cdbf715f97c1ed566
Brittan Fairchild <brittanfairchild@gmail.com>
2019-07-10 01:22:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 
 def my_hash_creator(key, value)
   ahash = {
-    key: value
+    ${key}: value
   }
   # return a hash that includes the key and value parameters passed into this method
   return ahash
