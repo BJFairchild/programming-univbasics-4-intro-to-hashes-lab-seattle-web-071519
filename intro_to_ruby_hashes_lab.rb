eee1a321753fffc5be4655aceb13657a8aa3e607
Brittan Fairchild <brittanfairchild@gmail.com>
2019-07-10 01:19:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  ahash = {}
+  ahash = {
+    ${key} = "${value}",
+  }
   # return a hash that includes the key and value parameters passed into this method
   return ahash
 end
